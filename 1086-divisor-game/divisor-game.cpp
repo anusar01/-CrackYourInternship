c3e4c79770f019d9760e053b17cf770128cc596b
Anusha Sarkar <113662816+anusar01@users.noreply.github.com>
2024-09-01 11:55:14 +0530

    Time: 6 ms (6.59%) | Memory: 7.6 MB (16.42%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     bool divisorGame(int n) {
+        //dp use krna hai
+        vector<int> dp(n+1,0);
+        dp[1]=0;//koi move hi nhi hai
+        for(int i=2;i<=n;i++){
+            for(int x=1;x<i;x++){
+                if(i%x==0 && dp[i-x]==0){
+                    dp[i]=1;
+                    break;//kyunki yahi mil gaya to aage next i ka dekh
+                }
+            }
+        }
+        return dp[n]==1;
+        
         //if even hai to bas alice hi jeet sakta but odd mai bob ka possible hai
-        if(n%2==0){
+        /*if(n%2==0){
             return true;
         }else{
             return false;
-        }
+        }*/
     }
 };
