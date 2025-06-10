1b8a237a6c23def9489282968b6292ffd78e128f
Anusha Sarkar <113662816+anusar01@users.noreply.github.com>
2025-06-10 11:04:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (30.86%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,36 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
         if (!head || !head->next || k == 0) return head;
-
-        // Step 1: Count nodes
-        ListNode* temp = head;
-        int count = 1;
-        while (temp->next != nullptr) {
-            temp = temp->next;
+        ListNode* temp=head;
+        int count=0;
+        while(temp!=nullptr){
             count++;
+            temp=temp->next;
         }
-
-        // Step 2: Make list circular
-        temp->next = head;
-
-        // Step 3: Find the new tail (at count - k % count - 1)
-        k = k % count;
-        if (k == 0) {
-            temp->next = nullptr; // break circular link
-            return head;
+        k=k%count;
+        if (k == 0) return head;
+        //move right
+        int shift=count-k;
+        ListNode* ptr=head;
+        while(shift>1){
+            ptr=ptr->next;
+            shift--;
         }
-
-        int stepsToNewTail = count - k;
-        ListNode* newTail = head;
-        while (--stepsToNewTail) {
-            newTail = newTail->next;
+        ListNode* nextptr=ptr->next;
+        ptr->next=nullptr;
+        ListNode* newhead=nextptr;
+        while(nextptr->next!=nullptr){
+            nextptr=nextptr->next;
         }
-
-        // Step 4: Break the circle
-        ListNode* newHead = newTail->next;
-        newTail->next = nullptr;
-
-        return newHead;
+        nextptr->next=head;
+        return newhead;    
     }
 };
