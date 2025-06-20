a412e4935a42965bd44d654b39b32504ac75d2a2
Anusha Sarkar <113662816+anusar01@users.noreply.github.com>
2025-06-20 12:07:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (56.56%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long res = 0;           // Use long to handle overflow
-        int sign = 1;
-        int c = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s[i] == ' ' && c == 0) {
-                continue; // Skip leading spaces
-            }
-            else if ((s[i] == '-' || s[i] == '+') && c == 0) {
-                sign = (s[i] == '-') ? -1 : 1;
-                c = 1; // Mark that sign/number has started
-            }
-            else if (isdigit(s[i])) {
-                c = 1; // Mark that number parsing started
+        long res=0;
+        int sign=1;
+        int c=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]==' '&& c==0){continue;}
+            else if((s[i]=='-'||s[i]=='+')&& c==0){sign = (s[i] == '-') ? -1 : 1;
+                c = 1;}
+            else if(isdigit(s[i])){
+                c=1;
                 int digit = s[i] - '0';
-                res = res * 10 + digit;
-
-                // Check for overflow
-                if (sign == 1 && res > INT_MAX) return INT_MAX;
-                if (sign == -1 && -res < INT_MIN) return INT_MIN;
-            }
-            else {
-                break; // Non-digit, non-space, non-sign char ends parsing
-            }
+                res=res*10+digit;
+                if(sign == 1 && res > INT_MAX) return INT_MAX;
+                if(sign == -1 && -res < INT_MIN) return INT_MIN;}
+            else{break;}
         }
-        return res * sign;
+        
+        return res*sign;
     }
 };
