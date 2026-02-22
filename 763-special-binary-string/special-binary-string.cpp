b8be67a83b3438909987b54b0c4ac754219e7386
Anusha Sarkar <113662816+anusar01@users.noreply.github.com>
2026-02-22 12:40:00 +0530

    Time: 2 ms (34.45%) | Memory: 9.3 MB (82.20%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string solve(string s){
+        vector<string> special;
+        int start=0;
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+            sum+=s[i]=='1'?1:-1;
+            if(sum==0){
+                special.push_back("1"+solve(s.substr(start+1,i-start-1))+"0");
+                start=i+1;
+            }
+            
+        } 
+        sort(begin(special),end(special),greater<string>());
+        string result;
+        for(string &str:special){
+            result+=str;
+        }
+        return result;
+    }
+    string makeLargestSpecial(string s) {
+        return solve(s);
+    }
+};
